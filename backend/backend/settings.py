7014a484fd553f2990660798c1a493137192c5f6
Farman Ali <farmanalifjk@gmail.com>
2026-04-01 09:20:16 +0500

    fix allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,9 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ronin-pk-landing-page-clone-production.up.railway.app"
+]
 
 
 # Application definition
